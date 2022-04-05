5186c879d0d0b8b8b259810e415d397e4c47f4ea
Ellemieke Van Kints <ellemiekevankints@gmail.com>
2022-04-05 13:32:22 -0400

    just need to separate data to 2 rows

--- ParseCSV.py
+++ ParseCSV.py
@@ -1,42 +1,48 @@
 # this file converts the PlanetAPI.csv to params.csv
 
 import pandas as pd
+import datetime
 import csv
 import math
+import time
 
 input = pd.read_csv(
     r'InputCSV/frame_index.csv')
 
-filename = pd.DataFrame(input, columns=['name'])
-timestamp = pd.DataFrame(input, columns=['datetime'])
+filename = input['name'].values.tolist()
 
-for i in timestamp.index:
-    row = timestamp['datetime'][i]
-    print(row)
+# convert datetime string to UNIX timestamp
+timestamp = input['datetime'].values.tolist()
+for i in range(len(timestamp)):
+    unixtime = time.mktime(datetime.datetime.strptime(timestamp[i], '%Y-%m-%dT%H:%M:%SZ').timetuple())
+    unixtime = int(unixtime)
+    timestamp[i] = unixtime
 
-xfov = 2.8 * (math.pi/180) # convert deg to rad
-yfov = 2.8 * (math.pi/180) # convert deg to rad
-xsize = 6.5 # nanometers
-ysize = 6.5 # nanometers
-foc = 3.6 # meters
+xfov = [2.8 * (math.pi/180), 2.8 * (math.pi/180)] # convert deg to rad
+yfov = [2.8 * (math.pi/180), 2.8 * (math.pi/180)] # convert deg to rad
+xsize = [6.5, 6.5] # nanometers
+ysize = [6.5, 6.5] # nanometers
+foc = [3.6, 3.6] # meters
 
+xpos = input['x_sat_eci'].values.tolist()
+ypos = input['y_sat_eci'].values.tolist()
+zpos = input['z_sat_eci'].values.tolist()
 
+# still need x, y, z rotation and x, y pixel count
+xrot = ['', '']
+yrot = ['', '']
+zrot = ['', '']
+xcount = ['', '']
+ycount = ['', '']
 
 columns = ['filename', 'x position', 'y position', 'z position', 'x rotation',
            'y rotation', 'z rotation', 'x field of view', 'y field of view',
            'camera focal length', 'x pixel well size', 'y pixel well size',
            'UNIX timestamp', 'x pixel count', 'y pixel count']
-output = []
+output = [filename, xpos, ypos, zpos, xrot, yrot, zrot, xfov, yfov, foc, xsize, ysize, timestamp, xcount, ycount]
 
 # create the params.csv file
-#with open('/Users/ellemiekevankints/Desktop/SSRL/PlanetAPI/PlanetAPI/OutputCSV/params.csv', 'w', encoding='UTF8', newline='') as params:
-    # create the csv writer
-    #writer = csv.writer(params)
-    
-    # write a row to the csv file
-    #writer.writerow(columns)
-    
-    # write the output data
-    #writer.writerown(output)
-    
+params = pd.DataFrame([output], columns=columns)
+params.to_csv('/Users/ellemiekevankints/Desktop/SSRL/PlanetAPI/PlanetAPI/OutputCSV/params.csv')
+
 
