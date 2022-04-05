248450ea5e817aed8fefc1974f010977897f89be
Ellemieke Van Kints <ellemiekevankints@gmail.com>
2022-04-05 17:45:45 -0400

    fixed some data type issues

--- ParseCSV.py
+++ ParseCSV.py
@@ -16,7 +16,8 @@ for i in range(len(timestamp)):
     unixtime = time.mktime(datetime.datetime.strptime(timestamp[i], '%Y-%m-%dT%H:%M:%SZ').timetuple())
     unixtime = int(unixtime)
     timestamp[i] = unixtime
-
+    print(timestamp[i])
+    
 xfov = [2.8 * (math.pi/180), 2.8 * (math.pi/180)] # convert deg to rad
 yfov = [2.8 * (math.pi/180), 2.8 * (math.pi/180)] # convert deg to rad
 xsize = [6.5, 6.5] # nanometers
@@ -34,12 +35,13 @@ zrot = ['', '']
 xcount = ['', '']
 ycount = ['', '']
 
+# data for params.csv file
 columns = ['filename', 'x position', 'y position', 'z position', 'x rotation',
            'y rotation', 'z rotation', 'x field of view', 'y field of view',
            'camera focal length', 'x pixel well size', 'y pixel well size',
            'UNIX timestamp', 'x pixel count', 'y pixel count']
-output = [[filename[0], xpos[0], ypos[0], zpos[0], xrot[0], yrot[0], zrot[0], xfov[0], yfov[0], foc[0], xsize[0], ysize[0], timestamp[0], xcount[0], ycount[0]],
-          [filename[1], xpos[1], ypos[1], zpos[1], xrot[1], yrot[1], zrot[1], xfov[1], yfov[1], foc[1], xsize[1], ysize[1], timestamp[1]], xcount[1], ycount[1]]
+output = [[filename[0], xpos[0], ypos[0], zpos[0], xrot[0], yrot[0], zrot[0], xfov[0], yfov[0], foc[0], xsize[0], ysize[0], str(timestamp[0]), xcount[0], ycount[0]],
+          [filename[1], xpos[1], ypos[1], zpos[1], xrot[1], yrot[1], zrot[1], xfov[1], yfov[1], foc[1], xsize[1], ysize[1], str(timestamp[1]), xcount[1], ycount[1]]]
 
 # create the params.csv file
 params = pd.DataFrame(output, columns=columns)
